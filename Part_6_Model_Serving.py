6d51b59e1763f6f812878eb7c5ab925bb26f14d5
Jeff Fletcher <jfletcher@cloudera.com>
2019-10-14 15:00:29 +0000

    Mostly Working

--- Part_6_Model_Serving.py
+++ Part_6_Model_Serving.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.ml import PipelineModel
-
+from pyspark.ml.classification import LogisticRegressionModel
 
 ## Note this a local Spark instance running in the engine
 spark = SparkSession.builder \
@@ -14,7 +14,8 @@ spark = SparkSession.builder \
       .config("spark.hadoop.yarn.resourcemanager.principal","jfletcher")\
       .getOrCreate()
   
-model = PipelineModel.load("s3a://ml-field/demo/flight-analysis/data/models/lr_model") 
+  
+model = PipelineModel.load("s3a://ml-field/demo/flight-analysis/data/models/large_model") 
 
 from pyspark.sql.types import *
 
@@ -23,6 +24,7 @@ StructField("ORIGIN", StringType(), True),
 StructField("DEST", StringType(), True),
 StructField("CRS_DEP_TIME", StringType(), True),
 StructField("CRS_ELAPSED_TIME", DoubleType(), True),
+StructField("WEEK", DoubleType(), True),
 StructField("DISTANCE", DoubleType(), True)])
 
 from pyspark.sql.types import StringType
@@ -30,7 +32,8 @@ from pyspark.sql.functions import udf,substring
 
 convert_time_to_hour = udf(lambda x: x if len(x) == 4 else "0{}".format(x),StringType())
 
-#args = {"feature":"AA,ICT,DFW,1135,85,328"}
+#args = {"feature":"AA,ICT,DFW,1135,11,85,328"}
+
 
 def predict(args):
   flight_features = args["feature"].split(",")
@@ -41,8 +44,10 @@ def predict(args):
     flight_features[2],
     flight_features[3],
     float(flight_features[4]),
-    float(flight_features[5]))],schema=feature_schema)
-  features = features.withColumn('CRS_DEP_HOUR', substring(convert_time_to_hour("CRS_DEP_TIME"),0,2))
+    float(flight_features[5]),
+    float(flight_features[6]))],schema=feature_schema
+  )
+  features = features.withColumn('CRS_DEP_HOUR', substring(convert_time_to_hour("CRS_DEP_TIME"),0,2).cast('float'))
   result = model.transform(features).collect()[0].prediction
   return {"result" : result}
 
